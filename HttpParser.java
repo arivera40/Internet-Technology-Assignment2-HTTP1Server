0430d11bab997bed4b62a1e12edcd4fb498fbaca
Andy J Rivera <rivera.andy40@gmail.com>
2020-11-19 21:10:03 -0500

    Update HttpParser.java

--- HttpParser.java
+++ HttpParser.java
@@ -1,15 +1,15 @@
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Locale;
 import java.util.Date;
 import java.util.TimeZone;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class HttpParser {
@@ -21,12 +21,16 @@ public class HttpParser {
 	private String lastModifiedStr;
 
 	private String payload;
-	
+	private String fromVariable;
+	private String userAgentVariable;
+	private int portNum;
+	private String serverName;
+
 	private File webroot;
 
 	final SimpleDateFormat form = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
 	
-	public HttpParser() {
+	public HttpParser(int portNum, String serverName) {
 		cmd = "";
 		resource = "";
 		version = 0;
@@ -34,7 +38,11 @@ public class HttpParser {
 		ifModified = null;
 		lastModifiedStr = "";
 
-		payload = null;
+		payload = "";
+		fromVariable = "";
+		userAgentVariable = "";
+		this.portNum = portNum;
+		this.serverName = serverName;
 
 		webroot = new File(".");
 	}
@@ -42,11 +50,6 @@ public class HttpParser {
 	//returns int to indicate status of request
 	public int parseRequest(String request) {
 		String[] parsedLine = request.split(" |\r\n");
-		// for(int i=0; i < parsedLine.length; i++){
-		// 	System.out.println(parsedLine[i]);
-		// }
-		// System.out.println();
-
 		if(parsedLine.length < 3) {
 			return -1;	// 400 Bad Request
 		}
@@ -73,19 +76,10 @@ public class HttpParser {
 					}
 				}
 			//Seems to be a POST with extra parameters and possible payload
-			}else if(parsedLine[3].equals("From:")){
-				if(!parsedLine[7].equals("Content-Type:") && !parsedLine[8].equals("application/x-www-form-urlencoded")){
-					return -4; // 500 Internal Server Error
-				}else if(!parsedLine[9].equals("Content-Length:")){
-					return -5; // 411 Length Required
-				}else{
-					//Has POST payload if Content-Length > 0
-					try{
-						if(Integer.parseInt(parsedLine[10]) > 0) payload = parsedLine[12];
-					}catch(NumberFormatException e){
-						return -5;	//411 Length Required when Content-Length value not numeric
-					}
-				}
+			}else if(parsedLine[0].equals("POST")){
+				int status = checkPOSTHeaders(parsedLine);
+				if(status == -4) return -4;	//500 Internal Server Error
+				if(status == -5) return -5;	//411 Length Required
 			}
 		}
 		
@@ -101,7 +95,7 @@ public class HttpParser {
 			return -1; // 400 Bad Request
 		}
 		
-		//Resource - Not sure if any format checking is necessary?
+		//Resource
 		resource = parsedLine[1];
 		
 		//Version number format checking
@@ -125,9 +119,39 @@ public class HttpParser {
 			}
 		}
 
-		if(payload != null) decodePayload();
-		System.out.println("payload is: " + payload);
+		if(cmd.equals("POST") && !resource.endsWith(".cgi")) return -6;
+		if(!payload.equals("")) decodePayload();
 		return 0;
+	}
+
+	//Checks existing POST headers returning an int code identifying any missing essential headers otherwise the content length itself
+	private int checkPOSTHeaders(String[] parsedLine){
+		boolean hasContentType = false;
+		boolean hasContentLength = false;
+		int contentLength = 0;
+		for(int i=3; i < parsedLine.length; i++){
+			if(parsedLine[i].equals("From:")){
+				fromVariable = parsedLine[i+1];
+				i++;
+			}else if(parsedLine[i].equals("User-Agent:")){
+				userAgentVariable = parsedLine[i+1];
+				i++;
+			}else if(parsedLine[i].equals("Content-Type:")){
+				hasContentType = true;
+			}else if(parsedLine[i].equals("Content-Length:")){
+				hasContentLength = true;
+				try{
+					if(Integer.parseInt(parsedLine[i+1]) > 0) payload = parsedLine[i+3];
+				}catch(NumberFormatException e){
+					return -5;
+				}
+				break;
+			}
+		}
+		if(!hasContentType) return -4;
+		if(!hasContentLength) return -5;
+
+		return contentLength;
 	}
 
 	//Helper function that decodes the encoded payload
@@ -144,6 +168,7 @@ public class HttpParser {
 		payload = decoded;
 	}
 	
+	//Retrieves MIME type of requested file
 	private String getMIME() {
 		if(resource.endsWith("html") || resource.endsWith("plain")) {
 			return "text/" + ((resource.endsWith("html")) ? "html" : "plain");
@@ -157,6 +182,7 @@ public class HttpParser {
 		}
 	}
 	
+	//Retrieves requested file contained within GET request
 	private byte[] getRequestedFile(File file, int length) {
 		byte[] data = new byte[length];
 		try {
@@ -171,38 +197,48 @@ public class HttpParser {
 	
 	public void getHttpResponse(PrintWriter head, BufferedOutputStream body) {
 		File file = new File(webroot, resource);
-		System.out.println(file.toString());
+		//Checks to see if file exists within webroot, otherwise responds with 404 Not Found
 		if(!file.exists()) {
 			head.println("HTTP/1.0 404 Not Found" + "\r\n");
 			head.flush();
 			return;
 		}
 
+		//Checks reading permission of existing file, if reading is not allowed responds with 403 Forbidden
 		if(!file.canRead()) {
 			head.println("HTTP/1.0 403 Forbidden" + "\r\n");
 			head.flush();
 			return;
 		}
+
 		int contentLength = (int) file.length();
 		Date lastModified = getModifiedDate(new Date(file.lastModified()));
-
 		String type = getMIME();
 
+		//Date object created for comparing purposes
 		Date expiration = new GregorianCalendar(2021, Calendar.OCTOBER, 2).getTime();
 		String expirationStr = form.format(expiration);
 		
 		if(cmd.equals("GET")) {
 			if(cmd.equals("GET") && ifModified != null) {
+				//Checks if file was last modified before the if modified header, if not then responds with 304 Not Modified
 				if(lastModified.before(ifModified)) {
-					head.print("HTTP/1.0 304 Not Modified" + "\r\n" + "Expires: " + expirationStr + "\r\n");
+					head.print("HTTP/1.0 304 Not Modified" + "\r\n" + 
+					"Expires: " + expirationStr + "\r\n");
 					head.flush();
 					return;
 				}
 			}
 			byte[] requestedFileData = getRequestedFile(file, contentLength);
-			head.print("HTTP/1.0 200 OK" + "\r\n" + "Content-Type: " + type + "\r\n" + "Content-Length: " + contentLength + "\r\n" + 
-						"Last-Modified: " + lastModifiedStr + "\r\n" + "Content-Encoding: identity" + "\r\n" +
-						"Allow: GET, POST, HEAD" + "\r\n" + "Expires: " + expirationStr + "\r\n\r\n");
+
+			//Outputs properly formatted HTTP GET response with the requested file
+			head.print("HTTP/1.0 200 OK" + "\r\n" + 
+			"Content-Type: " + type + "\r\n" + 
+			"Content-Length: " + contentLength + "\r\n" + 
+			"Last-Modified: " + lastModifiedStr + "\r\n" + 
+			"Content-Encoding: identity" + "\r\n" +
+			"Allow: GET, POST, HEAD" + "\r\n" + 
+			"Expires: " + expirationStr + "\r\n\r\n");
 			head.flush();
 			try {
 				body.write(requestedFileData, 0, contentLength);
@@ -211,17 +247,90 @@ public class HttpParser {
 				e.printStackTrace();
 			}		
 		}else if(cmd.equals("POST")){
-			CGIHandler cgiHandler = new CGIHandler(file.toString(), "");
-			System.out.println(cgiHandler.executeCGI());
+			String cgiOutput = executeCGI();
+			int outputLength = cgiOutput.length();
+
+			//Checks to see if cgi output returns any content, if not server responds with 204 No Content, otherwise proceeds with 200 OK as normal
+			if(outputLength < 1){
+				head.print("HTTP/1.0 204 No Content" + "\r\n");
+			}else{
+				head.print("HTTP/1.0 200 OK" + "\r\n");
+			}
+			//Outputs properly formatted HTTP POST response along with cgi output if content exists
+			head.print("Content-Type: " + "text/html" + "\r\n" +
+			"Content-Length: " + outputLength + "\r\n" + 
+			"Last-Modified: " + lastModifiedStr + "\r\n" +
+			"Content-Encoding: identity" + "\r\n" +
+			"Allow: GET, POST, HEAD" + "\r\n" +
+			"Expires: " + expirationStr + "\r\n\r\n");
+			if(outputLength >= 1) head.print(cgiOutput);
+			head.flush();
+
 		}else {
-			head.print("HTTP/1.0 200 OK" + "\r\n" + "Content-Type: " + type + "\r\n" + "Content-Length: " + contentLength + "\r\n" + 
-			"Last-Modified: " + lastModifiedStr + "\r\n" + "Content-Encoding: identity" + "\r\n" +
-			"Allow: GET, POST, HEAD" + "\r\n" + "Expires: " + expirationStr + "\r\n\r\n");
+			//Outputs properly formatted HTTP HEAD response
+			head.print("HTTP/1.0 200 OK" + "\r\n" + 
+			"Content-Type: " + type + "\r\n" + 
+			"Content-Length: " + contentLength + "\r\n" + 
+			"Last-Modified: " + lastModifiedStr + "\r\n" + 
+			"Content-Encoding: identity" + "\r\n" +
+			"Allow: GET, POST, HEAD" + "\r\n" + 
+			"Expires: " + expirationStr + "\r\n\r\n");
 			head.flush();
 		}
 		return;
 	}
+
+	//Loads environmental variables
+	private String [] setEnvironmentalVars(){
+		String [] variables = new String[6];
+		variables[0] = "CONTENT_LENGTH=" + payload.getBytes().length;
+		variables[1] = "SCRIPT_NAME=" + resource;
+		variables[2] = "SERVER_NAME=" + serverName;
+		variables[3] = "SERVER_PORT=" + portNum;
+		variables[4] = "HTTP_FROM=" + ((!fromVariable.equals("")) ? fromVariable : "null");
+		variables[5] = "HTTP_USER_AGENT=" + ((!userAgentVariable.equals("")) ? userAgentVariable : "null");
+		return variables;
+	}
+
+	//Executes cgi of requested file and returns the output
+	private String executeCGI(){
+		try{
+			Runtime run = Runtime.getRuntime();
+			String [] environmentVars = setEnvironmentalVars();
+
+			Process proc = run.exec("." + resource, environmentVars);
+			
+			if(!payload.equals("")){
+				proc.getOutputStream().write(payload.getBytes());
+				proc.getOutputStream().close();
+			}
+
+			InputStream stdInput = proc.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(stdInput));
+
+			StringBuilder output = new StringBuilder();
+
+			String line;
+			while((line = br.readLine()) != null){
+				output.append(line + "\n");
+			}
+
+			br.close();
+			stdInput.close();
+
+			String outputStr = output.toString();
+
+			if(outputStr.endsWith("\n\n")) return outputStr.substring(0, outputStr.length()-1);
+			
+			return outputStr;
+
+        }catch(IOException e){
+            e.printStackTrace();
+		}
+		return "Error";
+	}
 	
+	//Creates date object to compare for GET requests
 	synchronized private Date getModifiedDate(Date date) {
 		Date lastModified = null;
 		form.setTimeZone(TimeZone.getTimeZone("GMT"));
